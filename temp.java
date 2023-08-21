e6fe6edee585b025b684a98f67f30daf2b0bebad
hemanthnb1 <hemanthn@testvagrant.com>
2023-08-21 22:27:13 +0530

    second commit dev branch

--- temp.java
+++ temp.java
@@ -27,6 +27,10 @@ public class temp
         node root= new node(1);
         root.left=new node(2);
         root.right=new node(3);
+        root.left.left=new node(4);
+        root.left.right=new node(5);
+        root.right.left=new node(6);
+        root.right.right=new node(7);
         root.preOrderTraversal(root);
     }
 }
