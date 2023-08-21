27adc6ecd9053a3497be065ccfee3e305dee0f68
hemanthnb1 <hemanthn@testvagrant.com>
2023-08-21 22:22:30 +0530

    second commit

--- temp.java
+++ temp.java
@@ -1,6 +1,32 @@
+
+class node
+{
+    node left;
+    node right;
+    int data;
+    node(int data)
+    {
+        this.left=null;
+        this.right=null;
+        this.data=data;
+
+    }
+
+    void preOrderTraversal(node root)
+    {
+        if(root==null)
+        return;
+        System.out.println(root.data);
+        preOrderTraversal(root.left);
+        preOrderTraversal(root.right);
+    }
+}
 public class temp
 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        node root= new node(1);
+        root.left=new node(2);
+        root.right=new node(3);
+        root.preOrderTraversal(root);
     }
 }
